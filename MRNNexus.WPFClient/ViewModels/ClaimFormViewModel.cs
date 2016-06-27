2917bd2e39458317880abcad8923cea944022ebc
Alyssa Harvey <aharvey3869@gmail.com>
2016-06-27 16:06:52 -0400

    Added Submit and Cancel buttons to ClaimFormView/Model

--- MRNNexus.WPFClient/ViewModels/ClaimFormViewModel.cs
+++ MRNNexus.WPFClient/ViewModels/ClaimFormViewModel.cs
@@ -48,73 +48,84 @@ namespace MRNNexus.WPFClient.ViewModels
                 RaisePropertyChanged("ModifyLeadBtnIsEnabled");
             }
         }
-        public ObservableCollection<DTO_InsuranceCompany> InsuranceCompanies
-        {
-            get { return _insuranceCompanies; }
-            set
-            {
-                _insuranceCompanies = value;
-                RaisePropertyChanged("InsuranceCompanies");
-            }
-        }
+        //public ObservableCollection<DTO_InsuranceCompany> InsuranceCompanies
+        //{
+        //    get { return _insuranceCompanies; }
+        //    set
+        //    {
+        //        _insuranceCompanies = value;
+        //        RaisePropertyChanged("InsuranceCompanies");
+        //    }
+        //}
         #endregion
 
         #region Commands
-        public ICommand AttachLead
-        {
-            get { return new RelayCommand(new Action<object>(o => 
-                {
-                    //new AccountSelectView(2).ShowDialog();
-                    if(Lead != null && Lead.LeadID > 0)
-                    {
-                        AttachLeadBtnIsEnabled = false;
-                        AddNewLeadBtnIsEnabled = false;
-                        ModifyLeadBtnIsEnabled = true;
-                    }
-                }));
-            }
-        }
-        public ICommand AddNewLead
-        {
-            get
-            {
-                return new RelayCommand(new Action<object>(o =>
-                {
-                    LeadView leadView = new LeadView();
-                    leadView.SizeToContent = SizeToContent.WidthAndHeight;
-                    leadView.WindowStyle = WindowStyle.ThreeDBorderWindow;
-                    leadView.ResizeMode = ResizeMode.NoResize;
-                    leadView.ShowDialog();
+        //public ICommand AttachLead
+        //{
+        //    get { return new RelayCommand(new Action<object>(o => 
+        //        {
+        //            //new AccountSelectView(2).ShowDialog();
+        //            if(Lead != null && Lead.LeadID > 0)
+        //            {
+        //                AttachLeadBtnIsEnabled = false;
+        //                AddNewLeadBtnIsEnabled = false;
+        //                ModifyLeadBtnIsEnabled = true;
+        //            }
+        //        }));
+        //    }
+        //}
 
-                    if (Lead != null && Lead.LeadID > 0)
-                    {
-                        AttachLeadBtnIsEnabled = false;
-                        AddNewLeadBtnIsEnabled = false;
-                        ModifyLeadBtnIsEnabled = true;
-                    }
-                }));
-            }
-        }
-        public ICommand ModifyLead
-        {
-            get
-            {
-                return new RelayCommand(new Action<object>(o =>
-                {
-                    LeadView leadView = new LeadView();
-                    leadView.SizeToContent = SizeToContent.WidthAndHeight;
-                    leadView.WindowStyle = WindowStyle.ThreeDBorderWindow;
-                    leadView.ResizeMode = ResizeMode.NoResize;
-                    leadView.ShowDialog();
+        //public ICommand AddNewLead
+        //{
+        //    get
+        //    {
+        //        return new RelayCommand(new Action<object>(o =>
+        //        {
+        //            LeadView leadView = new LeadView();
+        //            leadView.SizeToContent = SizeToContent.WidthAndHeight;
+        //            leadView.WindowStyle = WindowStyle.ThreeDBorderWindow;
+        //            leadView.ResizeMode = ResizeMode.NoResize;
+        //            leadView.ShowDialog();
 
-                    if (Lead != null && Lead.LeadID > 0)
-                    {
-                        AttachLeadBtnIsEnabled = false;
-                        AddNewLeadBtnIsEnabled = false;
-                        ModifyLeadBtnIsEnabled = true;
-                    }
-                }));
-            }
+        //            if (Lead != null && Lead.LeadID > 0)
+        //            {
+        //                AttachLeadBtnIsEnabled = false;
+        //                AddNewLeadBtnIsEnabled = false;
+        //                ModifyLeadBtnIsEnabled = true;
+        //            }
+        //        }));
+        //    }
+        //}
+
+        //public ICommand ModifyLead
+        //{
+        //    get
+        //    {
+        //        return new RelayCommand(new Action<object>(o =>
+        //        {
+        //            LeadView leadView = new LeadView();
+        //            leadView.SizeToContent = SizeToContent.WidthAndHeight;
+        //            leadView.WindowStyle = WindowStyle.ThreeDBorderWindow;
+        //            leadView.ResizeMode = ResizeMode.NoResize;
+        //            leadView.ShowDialog();
+
+        //            if (Lead != null && Lead.LeadID > 0)
+        //            {
+        //                AttachLeadBtnIsEnabled = false;
+        //                AddNewLeadBtnIsEnabled = false;
+        //                ModifyLeadBtnIsEnabled = true;
+        //            }
+        //        }));
+        //    }
+        //}
+
+        public ICommand SubmitClaim
+        {
+            get { return new RelayCommand(new Action<object>(submitClaim)); }
+        }
+        public ICommand Cancel
+        {
+            get { return new RelayCommand(new Action<object>(cancel)); }
         }
         #endregion
 
@@ -122,6 +133,43 @@ namespace MRNNexus.WPFClient.ViewModels
         {
             InsuranceCompanies = new ObservableCollection<DTO_InsuranceCompany>(ServiceLayer.InsuranceCompaniesList);
         }
+
+        async public void submitClaim(object o)
+        {
+            if (Claim.ClaimID == 0) // If the claim doesn't exist (it should exist)
+            {
+                // Create the Claim
+                if((ErrorMessage = await new ServiceLayer().AddClaim(Claim.toDTO())) != null)
+                {
+                    return;
+                }
+
+                // Add it to the Claims list
+                Claims.Add(new Claim(ServiceLayer.Claim));
+            }
+            else if (Claim.ClaimID > 0) // If the Claim does exist (it should)
+            {
+                // Update the Claim
+                if((ErrorMessage = await new ServiceLayer().UpdateClaim(Claim.toDTO())) != null)
+                {
+                    return;
+                }
+
+                // Overwrite the original Claim in the Claims list with the updated information
+                Claim claim = Claims.Where(c => c.ClaimID == Claim.ClaimID).Single();
+                int index = Claims.IndexOf(claim);
+                Claims[index] = new Claim(ServiceLayer.Claim);
+            }
+        }
+
+        // Cancel any changes to the Claim and remove the page from the frame
+        public void cancel(object o)
+        {
+            Claim = Claims.Where(c => c.ClaimID == Claim.ClaimID).Single();
+            CurrentClaimPage = null;
+        }
+
+        
 
 
 
